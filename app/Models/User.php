9729684b43743c7a27be5280a72edc3f07265a7c
Edward Pitcher <epitcherbridel@gmail.com>
2022-05-08 12:41:24 +0100

    Add tailwindcss login

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'user_id';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+RateLimiter::for('global', function (Request $request) {
+    return $request->user()
+                ? Limit::perMinute(100)->by($request->user()->id)
+                : Limit::perMinute(10)->by($request->ip());
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('home', function () {
+    return view('home');
+})->name('home');
+
+
+Route::get('/hello', function() {
+    return 'Hello World';
+})->middleware('auth');
+
+ 
+Route::get('/user/{user:user_id}', function (User $user) {
+    return $user;
+});
+
+Route::middleware(['auth', 'throttle:10,1'])->group(function () {
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+
+    Route::get('/user/{user:user_id}', function (User $user) {
+        return $user;
+    });
+
 });
